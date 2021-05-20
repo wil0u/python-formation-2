80275265710ca76d321fc7747b1293bb5ef37216
Damien Garrouste <41282291+damiengarrouste78@users.noreply.github.com>
2021-05-20 15:17:24 +0200

    Add files via upload

---
+++ modules/operations/operation_basique.py
@@ -0,0 +1,17 @@
+def addition(valeur1, valeur2):
+    resultat = valeur1 + valeur2
+    return resultat
+
+def soustraction(valeur1, valeur2):
+    resultat = valeur1 - valeur2
+    return resultat
+
+def multiplication(valeur1, valeur2):
+    resultat = valeur1 * valeur2
+    return resultat
+
+def division(valeur1, valeur2):
+    resultat = valeur1 / valeur2
+    return resultat
+
+
