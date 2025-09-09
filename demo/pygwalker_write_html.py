23f5e91554838f8fd478441b03293b381a4afe01
wilverde <willeme.verdeaux@epsilon-france.com>
2025-09-09 19:13:45 +0200

    Changes !

--- demo/pygwalker_write_html.py
+++ demo/pygwalker_write_html.py
@@ -2,8 +2,8 @@ import pygwalker as pyg
 import pandas as pd
 
 
-df = pd.read_csv("https://kanaries-app.s3.ap-northeast-1.amazonaws.com/public-datasets/bike_sharing_dc.csv")
+df = pd.read_csv("../data/cereal/cereal_data.csv", sep=";")
 
-with open("pyg_demo_2.html", "w", encoding="utf-8") as f:
+with open("pyg_report.html", "w", encoding="utf-8") as f:
     html = pyg.to_html(df, spec="./gw_config.json")
     f.write(html)
